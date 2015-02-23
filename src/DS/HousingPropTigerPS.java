76a6df46ed4c37601208b65c5d5318b4e14a2764
amit.khandelwal <amit.khandelwal@kony.com>
2015-02-23 22:46:49 +0530

    modified two lines intersection codes

--- src/DS/HousingPropTigerPS.java
+++ src/DS/HousingPropTigerPS.java
@@ -1,13 +1,16 @@
 package DS;
 
-// this can be solved only by three conditions
+// this can be solved only by two conditions
+// modified the code with the correct conditions
+// just need to check that initial point of both the lines , comes in b/w the other lines
+
 public class HousingPropTigerPS {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        int v1=5,v2=10;
-       int v3=16,v4=18;
-       if((v3>=v1&&v3<=v2)||(v4>=v1&&v4<=v2)||(v3<=v1&&v4>=v1))
+       int v3=4,v4=26;
+       if((v3>=v1&&v3<=v2)||(v3<=v1&&v4>=v1))
     	   System.out.println("intersection");
        else
     	   System.out.println("false");
